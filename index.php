c9e9a608c45541c71786115bf2d3491ccba2997e
429273-7 <429273-7>
2019-12-16 16:37:41 +0300

    Hui

--- index.php
+++ index.php
@@ -3,22 +3,20 @@ ini_set("display_errors",1);error_reporting(-1);
 include_once("core/EquationInterface.php");
 include_once("core/LogInterface.php");
 include_once("core/LogAbstract.php");
-include_once("vm/VException.php");
-include_once("vm/Linear.php");
-include_once("vm/Square.php");
-include_once("vm/Log.php");
-
-$a = 1;
-$b = 2;
-$c = -3;
-
+include_once("v/VmException.php");
+include_once("v/Linear.php");
+include_once("v/Square.php");
+include_once("v/Log.php");
+$arr = [];
+$arr[] = readline("a=");
+$arr[] = readline("b=");
+$arr[] = readline("c=");
 try {
-	$solver = new vm\Square($a, $b, $c);
+	$solver = new Vm\Square($arr[0], $arr[1], $arr[2]);
 	
-	vm\Log::log("roots: " . implode(" ", $solver->ur2($a, $b, $c)));
-}catch(vm\VException $ex) {
-	vm\Log::log($ex->getMessage());
-}
-
-vm\Log::write();
+	Vm\Log::log("Roots: " . implode(", ", $solver->solve($arr[0], $arr[1], $arr[2])));
+}catch(Vm\VmException $ex) {
+	Vm\Log::log($ex->getMessage());
+} 
+Vm\Log::write();
 ?>
---
+++ v/VmException.php
@@ -0,0 +1,6 @@
+<?php
+namespace Vm;
+
+class VmException extends \RuntimeException {
+
+}
