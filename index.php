8520200a99af7d27f29af3bd9192885c9e3f9aac
429273-7 <429273-7>
2019-12-18 16:59:53 +0300

    WarMachine

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
-ini_set("display_errors",1);error_reporting(-1);
-include_once("core/EquationInterface.php");
-include_once("core/LogInterface.php");
-include_once("core/LogAbstract.php");
-include_once("vm/VmException.php");
-include_once("vm/Linear.php");
-include_once("vm/Square.php");
-include_once("vm/Log.php");
-$arr = [];
-$arr[] = readline("a=");
-$arr[] = readline("b=");
-$arr[] = readline("c=");
+
+include_once'core/EquationInterface.php';
+include_once'core/LogInterface.php';
+include_once'core/LogAbstract.php';
+include_once 'vm/Linear.php';
+include_once 'vm/Square.php';
+include_once 'vm/VmException.php';
+include_once 'vm/Log.php';
+
+$arr=array();
+
+vm\Log::log('Version '.file_get_contents('./version'));
+
+$arr[] = readline("a= ");
+$arr[] = readline("b= ");
+$arr[] = readline("c= ");
+
 try {
-	$solver = new Vm\Square($arr[0], $arr[1], $arr[2]);
-	
-	Vm\Log::log("Roots: " . implode(", ", $solver->solve($arr[0], $arr[1], $arr[2])));
-}catch(Vm\VmException $ex) {
-	Vm\Log::log($ex->getMessage());
-} 
-Vm\Log::write();
-?>
+    $solver = new vm\KvadrUr();
+	$roots = $solver->solve($arr[0], $arr[1], $arr[2]);
+
+    vm\Log::log("roots: " . implode(" , ", $roots));
+   
+}catch(vm\VmException $e) {
+
+    vm\Log::log($e->getMessage());
+}
+vm\Log::write();
--- vm/Log.php
+++ vm/Log.php
@@ -7,9 +7,11 @@ class Log extends \core\LogAbstract implements \core\LogInterface {
 	}
 
 	public function _write() {
-		foreach($this->log as $val) {
-			echo $val . "\n";
-		}
+		echo implode("\n", $this->log) . "\n";
+		$d = new DateTime();
+		if (!is_dir(__DIR__ . "/../Log/")) mkdir (__DIR__ . "/../Log/");
+		$name=__DIR__ . "/../Log/".$d->format("d.m.Y_H.i.s.u").".log";
+		file_put_contents($name, implode("\n\r", $this->log) . "\n\r");
 	}
 	
 	public static function write() {
